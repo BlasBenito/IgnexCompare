13dc9d66bca79ce885c4816115449040c5f93f5a
blasbenito <blasbenito@gmail.com>
2019-05-06 17:49:15 +0200

    fixed bug in DistanceMatrix

--- R/DistanceMatrix.R
+++ R/DistanceMatrix.R
@@ -89,33 +89,33 @@ DistanceMatrix <- function(sequences, method = NULL){
   #computing hellinger distance
   if (method %in% c("hellinger", "Hellinger", "HELLINGER", "Hell", "hell", "HELL")){
     for (i in 1:nrow.sequence.A-1){
-      distances.sequence.A[i] <- .HellingerDistance(x = sequence.A[i], y = sequence.A[i+1])
+      distances.sequence.A[i] <- .HellingerDistance(x = sequence.A[i, ], y = sequence.A[i+1, ])
     }
 
     for (j in 1:nrow.sequence.B-1){
-      distances.sequence.B[j] <- .HellingerDistance(x = sequence.B[j], y = sequence.B[j+1])
+      distances.sequence.B[j] <- .HellingerDistance(x = sequence.B[j, ], y = sequence.B[j+1, ])
     }
   }
 
   #computing euclidean distance
   if (method %in% c("euclidean", "Euclidean", "EUCLIDEAN", "euc", "Euc", "EUC")){
     for (i in 1:nrow.sequence.A-1){
-      distances.sequence.A[i] <- .EuclideanDistance(x = sequence.A[i], y = sequence.A[i+1])
+      distances.sequence.A[i] <- .EuclideanDistance(x = sequence.A[i, ], y = sequence.A[i+1, ])
     }
 
     for (j in 1:nrow.sequence.B-1){
-      distances.sequence.B[j] <- .EuclideanDistance(x = sequence.B[j], y = sequence.B[j+1])
+      distances.sequence.B[j] <- .EuclideanDistance(x = sequence.B[j, ], y = sequence.B[j+1, ])
     }
   }
 
   #computing chi distance
   if (method %in% c("chi", "Chi", "CHI", "chi.squared", "Chi.squared", "CHI.SQUARED")){
     for (i in 1:nrow.sequence.A-1){
-      distances.sequence.A[i] <- .ChiDistance(x = sequence.A[i], y = sequence.A[i+1])
+      distances.sequence.A[i] <- .ChiDistance(x = sequence.A[i, ], y = sequence.A[i+1, ])
     }
 
     for (j in 1:nrow.sequence.B-1){
-      distances.sequence.B[j] <- .ChiDistance(x = sequence.B[j], y = sequence.B[j+1])
+      distances.sequence.B[j] <- .ChiDistance(x = sequence.B[j, ], y = sequence.B[j+1, ])
     }
   }
 
